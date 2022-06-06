7c2ac35003c47b8c1e04747f56e4ecc4b1c4f330
Kevin Devlin <azwethinkweizkd@gmail.com>
2022-06-05 20:57:07 -0600

    migrated to redux-saga for users and categories

--- src/store/store.js
+++ src/store/store.js
@@ -6,8 +6,9 @@ import {
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
-import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
+import { rootSaga } from "./root-saga";
 import { rootReducer } from "./root-reducer";
 
 const persistConfig = {
@@ -15,11 +16,13 @@ const persistConfig = {
   storage,
   whitelist: ["cart"],
 };
+
+const sagaMiddleware = createSagaMiddleware();
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
   process.env.NODE_ENV !== "production" && logger,
-  thunk,
+  sagaMiddleware,
 ].filter(Boolean);
 
 const composedEnhancer =
@@ -35,5 +38,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
